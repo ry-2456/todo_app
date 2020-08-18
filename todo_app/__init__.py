200b436940fffd1146df6c7d5b85c63d65951f51
ry-2456 <ittanmomennimakaretyaimashita@gmail.com>
2020-08-19 00:02:56 +0900

    feat: MySQLに接続

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -1,5 +1,17 @@
+import os
 from flask import Flask
+# from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MySQL接続設定
+mysql = MySQL()
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = os.environ["MYSQL_ROOT_PASSWD"]
+app.config["MYSQL_DB"] = "flask_todo_app"
+mysql.init_app(app)
+
 
 from todo_app import views
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from todo_app import app
+from todo_app import app, mysql
 
 @app.route("/")
 def top():
@@ -7,7 +7,14 @@ def top():
 
 @app.route("/list")
 def show_list():
-    todo_list = ["歯を磨く", "結婚する", "大根を買う", "ピザと頼む"]
+    cur = mysql.connection.cursor()
+    sql = "SELECT * FROM task"
+    cur.execute(sql)
+    # mysql.connection.commit()
+    todo_list = cur.fetchall()
+    todo_list = [row[1] for row in todo_list]
+    cur.close()
+
     return render_template("list.html", todo_list=todo_list)
 
 @app.route("/edit")
