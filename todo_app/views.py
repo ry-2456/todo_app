92cee3a798ec66072fa6a90e8a05d30c2e6b914e
ry-2456 <ittanmomennimakaretyaimashita@gmail.com>
2020-08-19 11:07:00 +0900

    feat: タスクを追加できるようにした

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from todo_app import app, mysql
 
 @app.route("/")
@@ -7,13 +7,21 @@ def top():
 
 @app.route("/list")
 def show_list():
-    cur = mysql.connection.cursor()
-    sql = "SELECT * FROM task"
-    cur.execute(sql)
-    # mysql.connection.commit()
-    todo_list = cur.fetchall()
-    todo_list = [row[1] for row in todo_list]
-    cur.close()
+    """タスク一覧"""
+    with mysql.connection.cursor() as cur:
+        sql = "SELECT * FROM task"
+        cur.execute(sql)
+        # mysql.connection.commit() # delete, update, insertの場合のみ
+        todo_list = cur.fetchall()
+        todo_list = [row[1] for row in todo_list]
+
+    # cur = mysql.connection.cursor()
+    # sql = "SELECT * FROM task"
+    # cur.execute(sql)
+    # # mysql.connection.commit()
+    # todo_list = cur.fetchall()
+    # todo_list = [row[1] for row in todo_list]
+    # cur.close()
 
     return render_template("list.html", todo_list=todo_list)
 
@@ -25,6 +33,17 @@ def edit():
 def delete():
     pass
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "GET":
+        return render_template("add_task.html")
+
+    if request.method == "POST":
+        task_todo = request.form["task_todo"] 
+
+        with mysql.connection.cursor() as cur:
+            sql = "INSERT INTO task (todo) VALUES ('{}')".format(task_todo)
+            cur.execute(sql)
+            mysql.connection.commit() # delete, update, insertの場合のみ
+
+        return redirect(url_for("show_list"))
